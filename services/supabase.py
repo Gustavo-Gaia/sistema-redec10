48688761296f824274fc61d3cf1291f5785619f7
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-02-13 15:28:44 -0300

    Refactor Supabase client to use Streamlit secrets
    Updated Supabase client initialization to use Streamlit secrets.

---
+++ services/supabase.py
@@ -0,0 +1,9 @@
+# supabase_client.py
+from supabase import create_client
+import streamlit as st
+
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(url, key)
+
--- services/supabase_client.py
+++
@@ -1,8 +0,0 @@
-# supabase_client.py
-from supabase import create_client
-import os
-
-SUPABASE_URL = os.getenv("https://tfdmjadkmneslswsxwvg.supabase.co")
-SUPABASE_KEY = os.getenv("sb_publishable_szvWANlFRSVR6wylaI8sQw_n24x2-9_")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
